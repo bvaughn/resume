ba28556163dad3e3648146bbeff788af1b81a847
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-07-25 09:26:55 -0400

    Better mobile layout

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ if (url.searchParams.has("print")) {
   const downloadLink = document.querySelector(".download-link");
   downloadLink.style.display = "none";
 }
+
+function syncContactInfo(dataTestName) {
+  const headerElement = document.querySelector(
+    `[data-test-location="header"][data-test-name="${dataTestName}"]`
+  );
+  const sidebarElement = document.querySelector(
+    `[data-test-location="sidebar"][data-test-name="${dataTestName}"]`
+  );
+
+  if (headerElement && sidebarElement) {
+    headerElement.innerHTML = sidebarElement.innerHTML;
+  }
+}
+
+syncContactInfo("contact-location");
+syncContactInfo("contact-phone");
+syncContactInfo("contact-email");
+syncContactInfo("contact-github");
+syncContactInfo("contact-pdf");
